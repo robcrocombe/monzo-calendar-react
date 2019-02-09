e753d903dfa19f973a026358c06b8223f9af8039
Rob Crocombe <rjwc1993@yahoo.co.uk>
2019-02-09 00:23:21 +0000

    Convert classes to functions, use hooks

--- src/app.tsx
+++ src/app.tsx
@@ -1,31 +1,27 @@
 import * as React from 'react';
 import NavBar from './common/nav-bar';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div id="app" className="flex flex-column">
-        <main>
-          <NavBar />
-          {/* <Calendar /> */}
-        </main>
-        <footer>
-          <div className="has-text-centered">
-            <a
-              id="gh-link"
-              className="button is-white"
-              href="https://github.com/robcrocombe/monzo-calendar"
-              target="_blank"
-            >
-              <span className="has-text-grey">
-                github.com/robcrocombe/<strong>monzo-calendar</strong>
-              </span>
-            </a>
-          </div>
-        </footer>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div id="app" className="flex flex-column">
+      <main>
+        <NavBar />
+        {/* <Calendar /> */}
+      </main>
+      <footer>
+        <div className="has-text-centered">
+          <a
+            id="gh-link"
+            className="button is-white"
+            href="https://github.com/robcrocombe/monzo-calendar"
+            target="_blank"
+          >
+            <span className="has-text-grey">
+              github.com/robcrocombe/<strong>monzo-calendar</strong>
+            </span>
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
--- src/common/nav-bar.tsx
+++ src/common/nav-bar.tsx
@@ -1,76 +1,62 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 
-interface State {
-  showLoginButton?: boolean;
-  hasClientVars?: boolean;
-}
+export default function NavBar() {
+  const [showLoginButton, setShowLoginButton] = useState(true);
+  const [hasClientVars, setHasClientVars] = useState(false);
+  let loginButton: React.ReactNode;
 
-class NavBar extends React.Component<{}, State> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showLoginButton: true,
-      hasClientVars: false,
-    };
-  }
-
-  public render() {
-    let loginButton: React.ReactNode;
-    if (this.state.showLoginButton) {
-      if (this.state.hasClientVars) {
-        loginButton = (
-          <a className="button is-primary" href="monzoLoginUrl">
-            Login with Monzo
-          </a>
-        );
-      } else {
-        loginButton = (
-          <button type="button" className="button is-primary">
-            Login with Monzo
-          </button>
-        );
-      }
+  if (showLoginButton) {
+    if (hasClientVars) {
+      loginButton = (
+        <a className="button is-primary" href="monzoLoginUrl">
+          Login with Monzo
+        </a>
+      );
     } else {
       loginButton = (
-        <button type="button" className="button">
-          Log out
+        <button type="button" className="button is-primary">
+          Login with Monzo
         </button>
       );
     }
-
-    return (
-      <nav className="navbar has-shadow is-light">
-        <div className="navbar-brand">
-          <div className="navbar-item is-size-5 has-text-weight-bold">Monzo Calendar</div>
-        </div>
-        <div className="navbar-item">
-          Current Balance:&nbsp;
-          <span className="has-text-info">currentBalance</span>
-        </div>
-        <div className="navbar-item">
-          Planned Balance:&nbsp;
-          <span className="has-text-info">plannedBalance</span>
-        </div>
-        <div className="navbar-item" title="Profit/Loss">
-          P/L:&nbsp;
-          <span>diff</span>
-        </div>
-        <div className="navbar-end">
-          <div className="navbar-item">{loginButton}</div>
-        </div>
-        {/*<auth-modal
-          :visible="showAuthModal"
-          @close="closeAuthModal"
-          @submit="saveClientVars">
-        </auth-modal>
-        <logout-modal
-          :visible="showLogoutModal"
-          @close="closeLogoutModal"
-          @submit="logout">
-        </logout-modal>*/}
-      </nav>
+  } else {
+    loginButton = (
+      <button type="button" className="button">
+        Log out
+      </button>
     );
   }
-}
 
-export default NavBar;
+  return (
+    <nav className="navbar has-shadow is-light">
+      <div className="navbar-brand">
+        <div className="navbar-item is-size-5 has-text-weight-bold">Monzo Calendar</div>
+      </div>
+      <div className="navbar-item">
+        Current Balance:&nbsp;
+        <span className="has-text-info">currentBalance</span>
+      </div>
+      <div className="navbar-item">
+        Planned Balance:&nbsp;
+        <span className="has-text-info">plannedBalance</span>
+      </div>
+      <div className="navbar-item" title="Profit/Loss">
+        P/L:&nbsp;
+        <span>diff</span>
+      </div>
+      <div className="navbar-end">
+        <div className="navbar-item">{loginButton}</div>
+      </div>
+      {/*<auth-modal
+        :visible="showAuthModal"
+        @close="closeAuthModal"
+        @submit="saveClientVars">
+      </auth-modal>
+      <logout-modal
+        :visible="showLogoutModal"
+        @close="closeLogoutModal"
+        @submit="logout">
+      </logout-modal>*/}
+    </nav>
+  );
+}
